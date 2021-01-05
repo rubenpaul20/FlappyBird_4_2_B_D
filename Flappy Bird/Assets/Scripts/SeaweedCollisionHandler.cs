934b9c49101c499daedc26edb656e46cf9736986
rubenpaul20 <rubenpaul.bonnici@gmail.com>
2021-01-05 09:32:04 +0100

    Implemented Collisions
    Put collider on seaweed
    When fish collides with seaweed refresh scene

---
+++ Flappy Bird/Assets/Scripts/SeaweedCollisionHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SeaweedCollisionHandler : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Fish has entered the seaweed");
+        //Loud our game scene when the fish hits the seaweed
+        SceneManager.LoadScene(0);
+    }
+}
