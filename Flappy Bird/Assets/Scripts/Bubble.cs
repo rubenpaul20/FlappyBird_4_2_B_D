99a260e7748acc100ce9d9d9999060bf26148197
rubenpaul20 <rubenpaul.bonnici@gmail.com>
2021-01-19 10:15:01 +0100

    Made final adjustments to flappybird
    Added score
    Added extra bubbles for score
    Finished scoring system with seaweed and adjusted colliders on seaweed parent

---
+++ Flappy Bird/Assets/Scripts/Bubble.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * _speed);
+        //if the position on the y axis for the bubbke is greater than 10 we use reset.
+        if(transform.position.y > 10)
+        {
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        float randomHeight = Random.Range(-8f,-18f);
+        transform.position = new Vector3(transform.position.x,randomHeight,transform.position.z);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            //create a reference to our score script
+            ScoreKeeper scoreKeeper = GameObject.FindObjectOfType<ScoreKeeper>();
+            //check if we found the script
+            if(scoreKeeper !=null)
+            {
+                //increase the score
+                scoreKeeper.IncrementScore();
+            }
+            //reset the position of the bubble
+            Reset();
+        }
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    [SerializeField]
+    private int _currentScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void IncrementScore()
+    {
+        //_currentScore + 1
+        _currentScore++;
+        Text scoreText = GetComponent<Text>();
+        scoreText.text = "Score: " + _currentScore.ToString();
+    }
+}
